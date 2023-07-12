7603cea5659bfb46a634f56b1cf620fd7257d111
Julia Varfolomeeva <iuliiavarfolomeeva94a@gmail.com>
2023-07-12 18:04:42 +0300

    1-repository

---
+++ src/moks/index.ts
@@ -0,0 +1,317 @@
+export const users = [
+    {
+      "_id": "64aab24e87b4bb23dff404ea",
+      "index": 0,
+      "guid": "ced6e156-65a4-4b3e-8bff-25b265f1307d",
+      "isActive": true,
+      "balance": "$1,491.12",
+      "picture": "http://placehold.it/32x32",
+      "age": 20,
+      "eyeColor": "brown",
+      "name": "Rene Bailey",
+      "gender": "female",
+      "company": "MANGLO",
+      "email": "renebailey@manglo.com",
+      "phone": "+1 (828) 587-2422",
+      "address": "479 Ditmars Street, Bangor, Louisiana, 9209",
+      "about": "Fugiat laborum qui tempor esse minim excepteur. Labore amet occaecat irure nostrud sunt nisi duis. Ipsum nulla ad consequat excepteur velit ullamco velit elit. Laboris quis labore velit consectetur irure. Culpa irure nostrud ea magna et consequat tempor amet consequat duis incididunt sit excepteur cupidatat. Minim do ipsum incididunt incididunt eiusmod anim.\r\n",
+      "registered": "2022-10-01T10:44:50 -03:00",
+      "latitude": 2.368916,
+      "longitude": -133.614081,
+      "tags": [
+        "sunt",
+        "dolore",
+        "est",
+        "velit",
+        "deserunt",
+        "dolor",
+        "exercitation"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Wolf Lester"
+        },
+        {
+          "id": 1,
+          "name": "Keisha Barker"
+        },
+        {
+          "id": 2,
+          "name": "Abigail Walters"
+        }
+      ],
+      "greeting": "Hello, Rene Bailey! You have 7 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "64aab24ec0c90ee14f29d56a",
+      "index": 1,
+      "guid": "d6aff789-ad1a-411e-abb2-43b6a9615c2d",
+      "isActive": false,
+      "balance": "$1,757.55",
+      "picture": "http://placehold.it/32x32",
+      "age": 35,
+      "eyeColor": "green",
+      "name": "Lois Lawrence",
+      "gender": "female",
+      "company": "CAXT",
+      "email": "loislawrence@caxt.com",
+      "phone": "+1 (954) 524-3866",
+      "address": "465 Jackson Court, Corinne, Missouri, 140",
+      "about": "Labore irure anim ex minim. Sunt incididunt do aliquip esse culpa cillum exercitation reprehenderit. Sunt do minim id mollit labore veniam. Proident duis velit ea id. Minim ipsum sint quis consectetur aliqua officia proident quis nisi id minim laboris ad.\r\n",
+      "registered": "2020-11-10T01:15:42 -03:00",
+      "latitude": -75.831572,
+      "longitude": -82.138386,
+      "tags": [
+        "exercitation",
+        "cupidatat",
+        "deserunt",
+        "magna",
+        "commodo",
+        "aliquip",
+        "consectetur"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Knox Bruce"
+        },
+        {
+          "id": 1,
+          "name": "Sosa Cleveland"
+        },
+        {
+          "id": 2,
+          "name": "Dianna Sykes"
+        }
+      ],
+      "greeting": "Hello, Lois Lawrence! You have 5 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "64aab24e4bebec7d21d48e8f",
+      "index": 2,
+      "guid": "be5a2d57-ab13-4c7d-999e-e2f082654064",
+      "isActive": true,
+      "balance": "$1,007.82",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "brown",
+      "name": "Florence Dillard",
+      "gender": "female",
+      "company": "TELLIFLY",
+      "email": "florencedillard@tellifly.com",
+      "phone": "+1 (926) 527-2422",
+      "address": "281 Dorchester Road, Hatteras, Minnesota, 3628",
+      "about": "Commodo velit id Lorem id enim. Eu laboris ea culpa fugiat sint. Non deserunt voluptate commodo qui qui fugiat proident duis ipsum excepteur consectetur officia enim. Quis aliquip excepteur eiusmod sunt exercitation culpa commodo culpa culpa do tempor mollit irure ipsum. Esse enim officia id quis nisi reprehenderit dolor.\r\n",
+      "registered": "2020-08-12T07:43:30 -03:00",
+      "latitude": 56.264551,
+      "longitude": 102.55281,
+      "tags": [
+        "excepteur",
+        "anim",
+        "et",
+        "ullamco",
+        "non",
+        "tempor",
+        "magna"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Leila Humphrey"
+        },
+        {
+          "id": 1,
+          "name": "Kirsten Manning"
+        },
+        {
+          "id": 2,
+          "name": "Byrd Frost"
+        }
+      ],
+      "greeting": "Hello, Florence Dillard! You have 9 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "64aab24e5ebc8261c9923857",
+      "index": 3,
+      "guid": "57e780f8-825a-4ef2-868c-e4fadb6bb785",
+      "isActive": false,
+      "balance": "$2,211.69",
+      "picture": "http://placehold.it/32x32",
+      "age": 30,
+      "eyeColor": "brown",
+      "name": "Kellie Huff",
+      "gender": "female",
+      "company": "BUNGA",
+      "email": "kelliehuff@bunga.com",
+      "phone": "+1 (867) 492-3541",
+      "address": "184 Cypress Avenue, Klagetoh, Utah, 5046",
+      "about": "Tempor anim ullamco fugiat amet nulla. Eiusmod enim enim esse nisi enim. Lorem magna dolor in esse veniam cupidatat incididunt deserunt labore veniam esse laborum. Ipsum qui eiusmod magna commodo id ad duis incididunt commodo minim dolor. Et do minim est velit nostrud deserunt ullamco dolor ex ipsum. Non nostrud ipsum cillum occaecat mollit.\r\n",
+      "registered": "2017-11-01T10:59:54 -03:00",
+      "latitude": -88.004185,
+      "longitude": -37.388321,
+      "tags": [
+        "sunt",
+        "non",
+        "dolore",
+        "dolore",
+        "dolore",
+        "adipisicing",
+        "nisi"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "May Graves"
+        },
+        {
+          "id": 1,
+          "name": "Ayala Kaufman"
+        },
+        {
+          "id": 2,
+          "name": "Colleen Hess"
+        }
+      ],
+      "greeting": "Hello, Kellie Huff! You have 6 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "64aab24e0d8bb83d10b68b1e",
+      "index": 4,
+      "guid": "9c6c3e62-4009-4915-b514-0a4d4e17bfff",
+      "isActive": false,
+      "balance": "$3,250.86",
+      "picture": "http://placehold.it/32x32",
+      "age": 36,
+      "eyeColor": "green",
+      "name": "Kaitlin Callahan",
+      "gender": "female",
+      "company": "NETBOOK",
+      "email": "kaitlincallahan@netbook.com",
+      "phone": "+1 (893) 522-2421",
+      "address": "130 Hopkins Street, Advance, Oklahoma, 4778",
+      "about": "Deserunt Lorem nostrud nostrud tempor sint elit ut consequat ea mollit amet veniam culpa voluptate. Incididunt laborum consequat mollit sunt ipsum culpa. Velit ex cillum dolor sint minim ipsum. Veniam irure exercitation incididunt elit incididunt dolore et magna fugiat aliquip Lorem.\r\n",
+      "registered": "2022-01-11T02:34:09 -03:00",
+      "latitude": -28.16948,
+      "longitude": -133.827783,
+      "tags": [
+        "nisi",
+        "officia",
+        "esse",
+        "esse",
+        "laboris",
+        "amet",
+        "irure"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Howell Wolfe"
+        },
+        {
+          "id": 1,
+          "name": "Hutchinson Mendez"
+        },
+        {
+          "id": 2,
+          "name": "Underwood Horne"
+        }
+      ],
+      "greeting": "Hello, Kaitlin Callahan! You have 8 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "64aab24e018be57f583afd2e",
+      "index": 5,
+      "guid": "e0c0f9cb-f631-4d6b-a83a-86165133bc8e",
+      "isActive": false,
+      "balance": "$2,319.07",
+      "picture": "http://placehold.it/32x32",
+      "age": 37,
+      "eyeColor": "green",
+      "name": "Sweet Roth",
+      "gender": "male",
+      "company": "TETRATREX",
+      "email": "sweetroth@tetratrex.com",
+      "phone": "+1 (885) 490-2820",
+      "address": "787 Monroe Place, Indio, Virgin Islands, 7447",
+      "about": "Ea ea cillum adipisicing nulla id cillum in consequat aute. Mollit sunt magna exercitation ex nulla ullamco aute. In ipsum ea minim cillum eiusmod ullamco.\r\n",
+      "registered": "2014-11-21T10:21:23 -02:00",
+      "latitude": 86.83508,
+      "longitude": 88.899278,
+      "tags": [
+        "est",
+        "voluptate",
+        "magna",
+        "consequat",
+        "magna",
+        "id",
+        "culpa"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Casey Sheppard"
+        },
+        {
+          "id": 1,
+          "name": "Zamora Hurst"
+        },
+        {
+          "id": 2,
+          "name": "Theresa Guthrie"
+        }
+      ],
+      "greeting": "Hello, Sweet Roth! You have 6 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "64aab24ef3f5b200a53391e9",
+      "index": 6,
+      "guid": "19916137-0e7f-4912-98d7-54e57ac84f04",
+      "isActive": false,
+      "balance": "$3,798.48",
+      "picture": "http://placehold.it/32x32",
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Meyer Butler",
+      "gender": "male",
+      "company": "GLOBOIL",
+      "email": "meyerbutler@globoil.com",
+      "phone": "+1 (849) 515-2192",
+      "address": "692 Irving Street, Soudan, Rhode Island, 1966",
+      "about": "Veniam veniam qui irure dolor fugiat amet et cupidatat cupidatat labore mollit magna. Do minim duis ipsum minim consectetur id nulla ullamco. Culpa voluptate nulla do nulla voluptate et et ullamco. Cupidatat aliquip excepteur nostrud proident pariatur dolore. Consectetur sunt culpa ut qui dolore elit. Non nulla aliqua eiusmod aliqua amet aliqua mollit do mollit.\r\n",
+      "registered": "2023-01-07T09:04:21 -03:00",
+      "latitude": -64.632473,
+      "longitude": -29.712494,
+      "tags": [
+        "tempor",
+        "irure",
+        "minim",
+        "aliqua",
+        "dolore",
+        "nulla",
+        "culpa"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Bernadine Frye"
+        },
+        {
+          "id": 1,
+          "name": "Rutledge Conner"
+        },
+        {
+          "id": 2,
+          "name": "Hazel Zamora"
+        }
+      ],
+      "greeting": "Hello, Meyer Butler! You have 6 unread messages.",
+      "favoriteFruit": "banana"
+    }
+  ]
